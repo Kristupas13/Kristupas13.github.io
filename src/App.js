6bb1996e7b2a5f93e5a0f27047232d762a87d7b9
kristupas <lunskas@gmail.com>
2023-12-30 20:52:15 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import './App.css';
+import { ContactForm } from './components/feedback/ContactForm';
 import LeftSide from './components/left-side/LeftSide';
 import VideoBackground from './components/video-background/VideoBackground';
+import { FaFacebook } from 'react-icons/fa'; // Make sure to install the react-icons package
 
 function App() {
+  const redirectToFacebookPage = () => {
+    // Replace 'your-facebook-page-url' with the actual URL of your Facebook page
+    window.open('https://www.facebook.com/profile.php?id=61552746337962');
+  };
 
   return (
     <div className="App">
+      <div class="thumbnail-container">
+      <img src="3.jpg" alt="Company Thumbnail" class="thumbnail" />
+      </div>
       <VideoBackground />
       <div className='container'>
         <div className='left-column'>
           <LeftSide />
         </div>
+        <div className='middle-column'>
+          <img className='middle-image' alt='christmas book' src='1.jpg'/>
+        </div>
         <div className='right-column'>
-          Tesasssssssssssssssssssss
+          <ContactForm />
         </div>
       </div>
+      <footer className="footer" onClick={redirectToFacebookPage}>
+        Daugiau informacijos <FaFacebook className="facebook-icon" />
+      </footer>
     </div>
   );
 }
---
+++ src/components/feedback/ContactForm.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
+import "./ContactForm.css"; // Import your CSS file
+
+export function ContactForm() {
+  const [state, handleSubmit] = useForm("xwkgkpzz");
+  if (state.succeeded) {
+    return <p className="thank-you">Ačiū! Su jumis bus susisiekta</p>;
+  }
+
+  return (
+    <div className="parent">
+      <div className="test">
+      <div className="child">
+        Parašykite mums į <a target="_blank" rel="noreferrer" href="https://www.facebook.com/profile.php?id=61552746337962">Facebook paskyrą</a>
+        
+      </div>
+      <div className="or">
+        arba užpildykite formą
+      </div>
+      <form className="flex-form" onSubmit={handleSubmit}>
+        <div className="email-input">
+          <label htmlFor="email">Elektroninis paštas</label>
+          <input
+            className="custom-input"
+            id="email"
+            type="email"
+            name="email"
+          />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+        </div>
+
+        <textarea
+          className="text-area"
+          id="message"
+          name="message"
+          defaultValue="Noriu gauti Žiemos linksmybes!"
+        />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+        <button
+          className="christmas-button"
+          type="submit"
+          disabled={state.submitting}
+        >
+          Vykdyti
+        </button>
+      </form>
+      </div>
+
+    </div>
+  );
+}
